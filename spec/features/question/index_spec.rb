2d00526deb42ef6e5ff5b0c66b5b0a66eac26e2f
flllck <sadalexander11@gmail.com>
2022-03-21 02:22:22 +0300

    user can view list of questions

---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User can view the questions', %q{
+  In order to view questions
+  As an user
+  I'd like to be able to see questions
+} do
+
+  given!(:questions) { create_list(:question, 3) }
+  scenario 'User looks at questions' do
+    visit questions_path
+
+    questions.each { |question| expect(page).to have_content(question.title) }
+  end
+end
+
