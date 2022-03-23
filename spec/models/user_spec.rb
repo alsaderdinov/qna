2f7d2c989b4fb4ca0178ac4c684e6689b101aa93
flllck <sadalexander11@gmail.com>
2022-03-23 14:29:53 +0300

    fix: added test on 'author_of?' method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,3 +7,17 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 end
+
+describe 'user is author?' do
+  let(:user) { create(:user) }
+  let(:not_author) { create(:user) }
+  let(:question) { create(:question, user: user) }
+
+  it 'the user is author' do
+    expect(user).to be_author_of(question)
+  end
+
+  it 'the user is not the author' do
+    expect(not_author).to_not be_author_of(question)
+  end
+end
