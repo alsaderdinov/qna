05ccc2ba05b02e25a11c497670d6e1067bc9d4b6
flllck <sadalexander11@gmail.com>
2022-05-13 15:06:16 +0300

    daily digest email to all users with a list of questions created in the last 24 hours

--- spec/jobs/daily_digest_job_spec.rb
+++ spec/jobs/daily_digest_job_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe DailyDigestJob, type: :job do
-  let(:service) { double("Service::DailyDigset") }
+  let(:service) { double("DailyDigestService") }
 
   before do
     allow(DailyDigestService).to receive(:new).and_return(service)
   end
 
-  it 'calls Service::DailyDigest#send_digest' do
+  it 'calls DailyDigestService#send_digest' do
     expect(service).to receive(:send_digest)
     DailyDigestJob.perform_now
   end
